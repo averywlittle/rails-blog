fa0f4eec82cce782c53f52f412b571387d55202d
averywlittle <averywlittle@gmail.com>
2024-06-28 21:22:53 -0400

    destroy, and reuse code before controller actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,12 @@
 class BlogPostsController < ApplicationController
+    # Re-use the blog post find code and have it run before each action
+    before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
@@ -23,11 +25,9 @@ class BlogPostsController < ApplicationController
     end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post
         else
@@ -35,7 +35,16 @@ class BlogPostsController < ApplicationController
         end
     end
 
+    def destroy
+        @blog_post.destroy
+        redirect_to root_path
+    end
+
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
     end
 end
